fec692991c128217010046ce6d54f44be1ebfa13
aymanattar <aymanxman19999@gmail.com>
2023-04-17 04:01:52 -0700

    Done

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+
+const num1 = 2
+const num2 = 31
+const num3 = 4
+const num4 = 5
+const multiply = num1 * num2
+const random = Math.floor(Math.random() * 1000) + 1
+const mod = num3 % num4
+const max = Math.max(1, 2, 5, 10, 20)
